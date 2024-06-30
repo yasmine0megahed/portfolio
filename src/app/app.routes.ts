90bf8138c1ed742436675d1d1073e0aa91234db4
yasmine0megahed <yasmine.aboulmaaty.megahed@gmail.com>
2024-06-30 13:25:56 +0300

    feat: create home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ProfileComponent } from './component/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    title: 'Home page',
+  },
+];
