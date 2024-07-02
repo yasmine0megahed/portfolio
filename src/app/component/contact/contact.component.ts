fc9189f389667a258efe06612fdcb50eaa25e22f
yasmine0megahed <yasmine.aboulmaaty.megahed@gmail.com>
2024-07-02 13:51:11 +0300

    perf:contact component - profile component

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -5,25 +5,35 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-// functions
+  // functions
 
-// github
+  // github
   navigateToGitHub(): void {
     window.open('https://github.com/yasmine0megahed', '_blank');
   }
   //linkedin
-  navigateToLinkedin(){
+  navigateToLinkedin() {
     window.open('https://www.linkedin.com/in/yasminemegahed/', '_blank');
   }
   //facebook
-  navigateToFacebook(){
+  navigateToFacebook() {
     window.open('https://www.facebook.com/jessy.mjk', '_blank');
   }
   //instagram
-  navigateToInstagram(){
-    window.open('https://www.instagram.com/jessymjk?igsh=MTQ4M3J1ZDFjeHEwdA==', '_blank');
+  navigateToInstagram() {
+    window.open(
+      'https://www.instagram.com/jessymjk?igsh=MTQ4M3J1ZDFjeHEwdA==',
+      '_blank'
+    );
+  }
+  //cv
+  navigateToCv() {
+    window.open(
+      ' https://drive.google.com/file/d/1cC0C5b_WBnlH6DvqbEetdZV-Z4q-L_r5/view?usp=sharing',
+      '_blank'
+    );
   }
 }
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+//functions
+  //cv
+  navigateToCv() {
+    window.open(
+      ' https://drive.google.com/file/d/1cC0C5b_WBnlH6DvqbEetdZV-Z4q-L_r5/view?usp=sharing',
+      '_blank'
+    );
+  }
 }
